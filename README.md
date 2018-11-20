9cead5cd45b436fa07630ce210c1d9f66954ff87
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2018-11-20 02:10:52 -0700

    efinalized logic and css

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Word-Guess-Game
+# Word Guess Game
+This is a web browser hangman style game which was made using Vanilla JavaScript. 
+
+GitHub Page: https://nguyenchris.github.io/Word-Guess-Game/
+
+This game was primarly focused on implementing DOM manipulation and event listeners--specifically with `keyup` events. Considering this being my first ever project made with JavaScript from the ground up, I was able to organize all game code as an object. The only code outside of the object were functions that included event listeners and DOM manipulation in order to navigate through the single page game as well as start the game. 
+
+The theme of the game plays music by Drake where the user will guess what song is playing. 
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,87 @@
+// Initialize game object
 drakeGame = {
   wordToPick: {
     passion_fruit: {
-      name: "passionfruit",
       album: "assets/images/Nothing_was_same.jpg",
       song: "assets/audio/03_Passionfruit.mp3"
     },
-
     in_my_feelings: {
-      name: "inmyfeelings",
       album: "assets/images/scorpion.jpg",
       song: "assets/audio/in_my _feelings.mp3"
     },
     fake_love: {
-      name: "fakelove",
       album: "assets/images/Nothing_was_same.jpg",
       song: "assets/audio/fakelove.mp3"
     },
     jersey: {
-      name: "jersey",
       album: "assets/images/timetobealive.jpg",
       song: "assets/audio/jersey.mp3"
     },
     jumpman: {
-      name: "jumpman",
       album: "assets/images/timetobealive.jpg",
       song: "assets/audio/jumpman.mp3"
     },
     scholarships: {
-      name: "scholarships",
       album: "assets/images/timetobealive.jpg",
       song: "assets/audio/scholarships.mp3"
     },
     shot_for_me: {
-      name: "shotforme",
       album: "assets/images/takecare.jpg",
       song: "assets/audio/shotforme.mp3"
     },
     hold_on_were_going_home: {
-      name: "holdonweregoinghome",
       album: "assets/images/Nothing_was_same.jpg",
       song: "assets/audio/goinghome.mp3"
     },
     nice_for_what: {
-      name: "niceforwhat",
       album: "assets/images/scorpion.jpg",
       song: "assets/audio/niceForWhat.mp3"
+    },
+    one_dance: {
+      album: "assets/images/views.jpg",
+      song: "assets/audio/onedance.mp3"
+    },
+    controlla: {
+      album: "assets/images/views.jpg",
+      song: "assets/audio/controlla.mp3"
+    },
+    show_me_a_good_time: {
+      album: "assets/images/thankmelater.jpg",
+      song: "assets/audio/showmeagoodtime.mp3"
+    },
+    come_thru: {
+      album: "assets/images/Nothing_was_same.jpg",
+      song: "assets/audio/comethru.mp3"
+    },
+    know_yourself: {
+      album: "assets/images/toolate.jpg",
+      song: "assets/audio/knowyourself.mp3"
+    },
+    find_your_love: {
+      album: "assets/images/thankmelater.jpg",
+      song: "assets/audio/findyourlove.mp3"
+    },
+    gods_plan: {
+      album: "assets/images/scorpion.jpg",
+      song: "assets/audio/godsplan.mp3"
+    },
+    crew_love: {
+      album: "assets/images/takecare.jpg",
+      song: "assets/audio/crewlove.mp3"
+    },
+    pound_cake_paris_morton_music: {
+      album: "assets/images/Nothing_was_same.jpg",
+      song: "assets/audio/poundcake.mp3"
     }
   },
 
+  songsToArr: [],
   guessedLettersArr: [],
   letters: [],
   wordToGuess: null,
   wordToGuessObj: {},
   wordToGuessArr: [],
-  wordToGuessStr: null,
-  guessedLetter: null,
-  guessesLeft: 10,
+  guessesLeft: 8,
   matchedLetters: [],
   songsPlayed: [],
   wins: 0,
@@ -63,26 +89,30 @@ drakeGame = {
 
   genWordToPick: function() {
     //create array of the object wordToPick containing song objects that are all within the drakeGame object
-    var songsToArr = Object.keys(this.wordToPick);
+    this.songsToArr = Object.keys(this.wordToPick);
 
     // choose random song from the array and assign to wordToGuess property
-    this.wordToGuess =
-      songsToArr[Math.floor(Math.random() * songsToArr.length)];
+    this.wordToGuess = this.songsToArr[
+      Math.floor(Math.random() * this.songsToArr.length)
+    ];
 
-    // assign property wordToGuessObj in order to later call the properties of the random song's object properties in other functions
+    // assign property wordToGuessObj in order to later select the properties of the random song's object properties
     this.wordToGuessObj = this.wordToPick[this.wordToGuess];
 
+    // now that the random song is chosen, check to see if it has already been played yet
     this.checkIfPlayed();
-
-    // this.setupGame();
   },
 
   checkIfPlayed: function() {
+    // check if the song has been played already by seeing if the song is in the songsPlayed array
     if (this.songsPlayed.indexOf(this.wordToGuess) == -1) {
+      // if it hasn't, add the song to the songsPlayed array
       this.songsPlayed.push(this.wordToGuess);
+      // separate the wordToGuess string between every character and assign it to wordToGuessArr property
       this.wordToGuessArr = this.wordToGuess.split("");
       this.setupGame();
     } else {
+      // if the song has already played, generate another random song
       this.genWordToPick();
     }
   },
@@ -95,17 +125,13 @@ drakeGame = {
       var textnode = document.createTextNode("");
       node.appendChild(textnode);
 
-      // determine if the word to guess has any spaces. If so, add classes for style with the hiddenDIV class which hides the box from the game page. If not, display the box with the class that styles the box. Also add the index of the letter from the wordToGuess array to each div to determine which letter will go in the box when the user enters the correct letter
+      // determine if the word to guess has any spaces
       if (this.wordToGuessArr[i] == "_") {
-        document
-          .querySelector(".letterDisplay")
-          .appendChild(node)
-          .classList.add("letterBox", "hiddenDIV");
+        // If so, add classes for style with the hiddenDIV class which hides the empty box from the game page
+        document.querySelector(".letterDisplay").appendChild(node).classList.add("letterBox", "hiddenDIV");
       } else {
-        document
-          .querySelector(".letterDisplay")
-          .appendChild(node)
-          .classList.add("letterBox", "letterInBox" + i);
+        // If no space, display the box with the class that styles the box. Also add the index of the letter from the wordToGuess array to each div as an html class in order to determine which letter will go in the box when the user enters the correct letter
+        document.querySelector(".letterDisplay").appendChild(node).classList.add("letterBox", "letterInBox" + i);
       }
     }
 
@@ -119,21 +145,12 @@ drakeGame = {
     // play the song in background depending on which random song/word is chosen
     var x = document.getElementById("myAudio");
     x.src = this.wordToGuessObj.song;
-    x.play();
     x.controls = false;
+    x.currentTime = 20;
+    x.play();
 
     // display album art
     document.querySelector(".album-art").src = this.wordToGuessObj.album;
-  },
-
-  // create all letters from a to z in an array
-  genLettersArr: function(charA, charZ) {
-    var a = charA.charCodeAt(0);
-    var z = charZ.charCodeAt(0);
-    for (; a <= z; a++) {
-      this.letters.push(String.fromCharCode(a));
-    }
-    return this.letters;
   },
 
   checkGuess: function(letter) {
@@ -154,19 +171,17 @@ drakeGame = {
             // select the class name based on the index of the word to guess array and display the letter in the letter box
             document.querySelector(".letterInBox" + i).textContent = letter;
 
-            // add letter to array of mattched letters at the same index as the word to guess array
+            // add letter to array of matched letters
             this.matchedLetters.push(letter);
           }
         }
         this.checkGame();
       }
     } else {
+      // add incorrect guessed letter to array of guessed letters
       this.guessedLettersArr.push(letter);
       this.guessesLeft--;
-      document.querySelector(
-        "#lettersGuessed"
-      ).textContent = this.guessedLettersArr.join(", ");
-      document.querySelector("#remainingGuess").textContent = this.guessesLeft;
+      this.updateGame();
       this.checkGame();
     }
   },
@@ -176,25 +191,29 @@ drakeGame = {
     if (this.matchedLetters.length == this.wordToGuessArr.length) {
       // add a point to wins
       this.wins++;
+      // display text indicating the user guessed correctly and restart game
       this.guessedCorrect();
-
-      // if there are no more guesses left..
     }
 
+    // if there are no more guesses left..
     if (this.guessesLeft == 0) {
-      // point added to losses and game restarts
+      // point added to losses
       this.losses++;
-      this.restartGame();
+      // update page on HTML
+      this.updateGame();
+      // display that the user guessed incorrect and restart the game
+      this.guessedIncorrect();
     }
   },
 
   alreadyGuessed: function() {
+    // display on page that they already guessed that letter
     var x = document.querySelector(".alreadyGuessed");
     x.style.display = "block";
     x.textContent = "You already guessed that!";
     setTimeout(function() {
       x.style.display = "none";
-    }, 1000);
+    }, 600);
   },
 
   guessedCorrect: function() {
@@ -203,7 +222,21 @@ drakeGame = {
     x.style.display = "block";
     x.textContent = "You guessed the word right!";
     setTimeout(function() {
-      x.textContent = "Now onto the next song...Get ready!";
+      x.textContent = "Now onto the next song...good luck!";
+    }, 1000);
+    setTimeout(function() {
+      x.style.display = "none";
+    }, 2500);
+    setTimeout(this.restartGame(), 2600);
+  },
+
+  guessedIncorrect: function() {
+    // display on page that they guessed the word incorrectly
+    var x = document.querySelector(".alreadyGuessed");
+    x.style.display = "block";
+    x.textContent = "Aw, you guessed that song wrong...better luck next time!";
+    setTimeout(function() {
+      x.textContent = "Now onto the next song...you got this!";
     }, 1500);
     setTimeout(function() {
       x.style.display = "none";
@@ -211,36 +244,76 @@ drakeGame = {
     setTimeout(this.restartGame(), 2600);
   },
 
-  restartGame: function() {
-    this.guessedLettersArr = [];
-    this.guessesLeft = 10;
-    this.matchedLetters = [];
-
+  updateGame: function() {
     document.querySelector("#remainingGuess").textContent = this.guessesLeft;
     document.querySelector("#wins").textContent = this.wins;
     document.querySelector("#losses").textContent = this.losses;
-    document.querySelector(
-      "#lettersGuessed"
-    ).textContent = this.guessedLettersArr;
-    var boxes = document.querySelector(".letterDisplay");
-    while (boxes.hasChildNodes()) {
-      boxes.removeChild(boxes.firstChild);
+    document.querySelector("#lettersGuessed").textContent = this.guessedLettersArr.join(", ");
+  },
+
+  genLettersArr: function(charA, charZ) {
+    // create all letters from a to z in an array to prevent non-letter keys being considered a guess
+    var a = charA.charCodeAt(0);
+    var z = charZ.charCodeAt(0);
+    for (; a <= z; a++) {
+      this.letters.push(String.fromCharCode(a));
     }
-    this.genWordToPick();
+    return this.letters;
+  },
+
+  gameOver: function() {
+    this.updateGame();
+    // remove game page elements besides scoreboard, header, and footer
+    var rows = document.querySelectorAll(".row");
+    for (i = 0; i < rows.length; i++) {
+      rows[i].style.display = "none";
+    }
+
+    var gameEnd = document.querySelector(".gameOver");
+    gameEnd.style.display = "block";
+
+    if (this.wins > this.losses) {
+      gameEnd.textContent = "You won!";
+    } else if (this.wins == this.losses)
+      gameEnd.textContent = "Dang, you tied!";
+    else {
+      gameEnd.textContent = "You Lost :/";
+    }
+  },
+
+  restartGame: function() {
+    // check if all songs have been played
+    if (this.songsPlayed.length === this.songsToArr.length) {
+      // if so, display game over page
+      this.gameOver();
+
+      // if not, reset guesses and matched letters
+    } else {
+      this.guessedLettersArr = [];
+      this.guessesLeft = 8;
+      this.matchedLetters = [];
+
+      // remove letter boxes on game page
+      var boxes = document.querySelector(".letterDisplay");
+      while (boxes.hasChildNodes()) {
+        boxes.removeChild(boxes.firstChild);
+      }
+      // update game page and generate new word to guess
+      this.updateGame();
+      this.genWordToPick();
+    }
   }
 };
+
+
 
 // click event listener for start button in order to render game page
 document.querySelector(".splash-btn").addEventListener("click", splash);
 
 // render HTML to show game page with directions after user clicks play button from splash page
 function splash() {
-  document
-    .querySelector(".splash")
-    .classList.add("animation", "fadeOut", "hide");
-  document
-    .querySelector(".game-page")
-    .classList.add("animation", "fadeIn", "active");
+  document.querySelector(".splash").classList.add("animation", "fadeOut", "hide");
+  document.querySelector(".game-page").classList.add("animation", "fadeIn", "active");
   keyStart();
 }
 
@@ -253,19 +326,16 @@ function gameStart() {
   //remove the keyStart function which contains the event listener in order to prevent 2 keyup event listeners on document
   document.removeEventListener("keyup", gameStart);
   // display actual game on HTML and remove directions from game page
-  document
-    .querySelector(".game-start")
-    .classList.add("animation", "fadeIn", "active");
+  document.querySelector(".game-start").classList.add("animation", "fadeIn", "active");
   document.querySelector(".directions").style.display = "none";
 
+  // intialize game by generating the random word to guess and letters that will be available as guesses
   drakeGame.genWordToPick();
   drakeGame.genLettersArr("a", "z");
-  // testing code for autoplay audio after click
 
   // listen for keyup events
   document.addEventListener("keyup", function(e) {
     var key = e.key;
-    // document.querySelector('#lettersGuessed').innerHTML = key;
 
     // check if key that is pressed is a letter. If so, use the letter that is pressed for the checkGuess function
     if (drakeGame.letters.indexOf(key) !== -1) {
